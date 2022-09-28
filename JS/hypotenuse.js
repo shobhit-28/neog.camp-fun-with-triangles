ec4c0066b95034c524f4889c6c3d624421043b9c
shobhit-28 <shobhitraj34@gmail.com>
2022-09-28 20:39:46 +0530

    hypotenuse html file done

---
+++ JS/hypotenuse.js
@@ -0,0 +1,10 @@
+let base = document.querySelector('#base');
+let height = document.querySelector('#height');
+
+let btn = document.querySelector('.btn');
+
+let declaration = document.querySelector('.declaration')
+
+btn.addEventListener('click', function(){
+
+})
