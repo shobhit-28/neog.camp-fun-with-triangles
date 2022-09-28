c5eee1a680d29ced8507b6d19a08e28ba487564f
shobhit-28 <shobhitraj34@gmail.com>
2022-09-29 03:12:23 +0530

    style: hypotenuse

--- JS/hypotenuse.js
+++ JS/hypotenuse.js
@@ -9,3 +9,7 @@ btn.addEventListener('click', function(){
     let hypotenuse = Math.sqrt((base.value*base.value)+(height.value*height.value));
     declaration.innerText = 'The length of hypotenuse is ' + hypotenuse;
 })
+
+let today = new Date();
+let year = document.querySelector('.year');
+year.innerText = today.getDate() + '/' + (today.getMonth()+1) + '/'  + today.getFullYear();
