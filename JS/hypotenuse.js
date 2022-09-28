ca79b26cca7ff1da28ca89ca05ab48fc277fe437
shobhit-28 <shobhitraj34@gmail.com>
2022-09-28 20:56:00 +0530

    hypotenuse js done

--- JS/hypotenuse.js
+++ JS/hypotenuse.js
@@ -6,5 +6,6 @@ let btn = document.querySelector('.btn');
 let declaration = document.querySelector('.declaration')
 
 btn.addEventListener('click', function(){
-
+    let hypotenuse = Math.sqrt((base.value*base.value)+(height.value*height.value));
+    declaration.innerText = 'The length of hypotenuse is ' + hypotenuse;
 })
